25854393d2aa23e3673caedc8db961acb1bb6ac5
Yurii Kosach <81816470+YurKo22@users.noreply.github.com>
2022-07-15 17:13:22 +0300

    1.2.0
    Adaptive section About

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(function(){
    $('.menu__btn').on('click', function(){
-      $('.menu__list').toggleClass('menu__list--active');
+      $('.header__top-inner').toggleClass('header__top-inner--active');
    });
+   
    
    var mixer = mixitup('.portfolio__content');
 });
