fed82fe4553af2ee83161eb74c4ce1d4b61b3511
Yurii Kosach <81816470+YurKo22@users.noreply.github.com>
2022-07-20 12:23:34 +0300

    1.3.1
    Sticky scroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 $(function(){
-   $('.menu__btn').on('click', function(){
+	$(".menu a").on("click", function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+	   $('body,html').animate({scrollTop: top}, 1500);
+	});
+	$(".header a").on("click", function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+	   $('body,html').animate({scrollTop: top}, 1500);
+	});
+
+
+   $('.menu__btn, .menu a').on('click', function(){
       $('.header__top-inner').toggleClass('header__top-inner--active');
    });
    
-   
+
    var mixer = mixitup('.portfolio__content');
 });
