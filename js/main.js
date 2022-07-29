eb0a9ef77d78637b51abcfa176c857969db1394c
Yurii Kosach <81816470+YurKo22@users.noreply.github.com>
2022-07-29 07:08:28 +0300

    1.3.3

--- js/main.js
+++ js/main.js
@@ -16,7 +16,8 @@ $(function(){
    $('.menu__btn, .menu a').on('click', function(){
       $('.header__top-inner').toggleClass('header__top-inner--active');
    });
-   
+
+
 
    var mixer = mixitup('.portfolio__content');
 });
