64e922238a9eff9164b4ba66c3786049f78c3b42
Yurii Kosach <81816470+YurKo22@users.noreply.github.com>
2022-07-29 14:48:17 +0300

    1.4
    Different edits

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 $(function(){
-	$(".menu a").on("click", function (e) {
-		e.preventDefault();
-		var id  = $(this).attr('href'),
-		top = $(id).offset().top;
-	   $('body,html').animate({scrollTop: top}, 1500);
+	$(window).scroll(function () {
+		if ($(window).scrollTop() >= 200) {
+			$('.header').addClass('sticky')
+		} else {
+			$('.header').removeClass('sticky')
+	    }
 	});
-	$(".header a").on("click", function (e) {
+
+	const headerHeight = $('.header').outerHeight();
+
+	$(".menu a, .main a, footer a").on("click", function (e) {
 		e.preventDefault();
 		var id  = $(this).attr('href'),
 		top = $(id).offset().top;
@@ -14,10 +18,8 @@ $(function(){
 
 
    $('.menu__btn, .menu a').on('click', function(){
-      $('.header__top-inner').toggleClass('header__top-inner--active');
+      $('.menu').toggleClass('menu--active');
    });
-
-
 
    var mixer = mixitup('.portfolio__content');
 });
